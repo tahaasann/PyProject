d96d0d1207ea56f272d886e1bab2d18e555ab532
Taha S. ASAN <tahaasann@gmail.com>
2024-06-08 20:32:10 +0300

    Most repeated sentences feature has been added.

--- app.py
+++ app.py
@@ -9,35 +9,49 @@ def word_counter_text_analysis(text):
     return sequential_words
 
 def word_emulation(words, n):
-    words1, words2, words3 = words, words[1:], words[2:]
     words_nli = zip(*[words[i:] for i in range(n)])
     word_counter = Counter(words_nli)
     sequential_words = sorted(word_counter.items(), key=lambda x: x[1], reverse=True)
     return sequential_words
 
-file_name = 'your-file.txt'
+def sentence_counter_text_analysis(text):
+    sentences = re.split(r'[.!?]', text)
+    sentences = [sentence.strip().lower() for sentence in sentences if sentence.strip()]
+    sentence_counter = Counter(sentences)
+    sequential_sentences = sorted(sentence_counter.items(), key=lambda x: x[1], reverse=True)
+    return sequential_sentences
+
+file_name = 'words.txt'
 
 try:
     with open(file_name, 'r', encoding='utf-8') as file:
         text = file.read()
         words = text.split()
+        
+        # Kelime analizi
         word_list = word_counter_text_analysis(text)
         binary_word_list = word_emulation(words, 2)
         triple_word_list = word_emulation(words, 3)
         
-        print("Total number of words:", len(words))
-        print("Total number of different words:", len(word_list))
-        print("Most used words:")
-        for word, number in word_list[:10]:
-            print(f"'{word}' word used {number} times")
+        # Cümle analizi
+        sentence_list = sentence_counter_text_analysis(text)
         
-        print("\nThe most used binary phrases:")
-        for binary, number in binary_word_list[:10]:
-            print(f"'{' '.join(binary)}' phrase used {number} times")
+        # Sonuçları yazdırma
+        #print("En çok kullanılan kelimeler:")
+        #for word, number in word_list[:10]:
+        #    print(f"{word}: {number}")
+        
+        #print("\nEn çok kullanılan ikili kelime grupları:")
+        #for binary, number in binary_word_list[:10]:
+        #    print(f"{' '.join(binary)}: {number}")
             
-        print("\nThe most used triplet phrases:")
-        for triple, number in triple_word_list[:10]:
-            print(f"'{' '.join(triple)}' phrase used {number} times")
+        #print("\nEn çok kullanılan üçlü kelime grupları:")
+        #for triple, number in triple_word_list[:10]:
+        #    print(f"{' '.join(triple)}: {number}")
+            
+        print("\nEn çok kullanılan cümleler:")
+        for sentence, number in sentence_list[:10000]:
+            print(f"{sentence}: {number}")
+
 except FileNotFoundError:
     print(f"{file_name} file not found")
-
